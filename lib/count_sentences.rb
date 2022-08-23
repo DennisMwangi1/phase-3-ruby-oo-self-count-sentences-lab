64233838b6949a8b3ebbc2169ad45c01957ee27d
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-08-23 13:09:52 +0300

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,33 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".") 
-      true
-    else
-      false
-    end
+    self.end_with?(".") 
   end
 
   def question?
-    if self.end_with?("?") 
-      true
-    else
-      false
-    end
+    self.end_with?("?") 
   end
 
   def exclamation?
-    if self.end_with?("!") 
-      true
-    else
-      false
-    end
+     self.end_with?("!") 
   end
 
   def count_sentences
     split = self.split(/\.|\?|\!/).filter { |sentence| !sentence.empty? }.size
-    
-   
   end
 end
 
